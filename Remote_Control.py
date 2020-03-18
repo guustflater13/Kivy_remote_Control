f39fa8deddc84abd5d77bf22f1d3e6f1de89264a
Tjerk <tjerk.schuringa@gmail.com>
2020-03-18 20:50:57 +0100

    color scheme button action change values on button press

--- Remote_Control.py
+++ Remote_Control.py
@@ -2,6 +2,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import StringProperty
+from kivy.properties import NumericProperty
 from kivy.clock import Clock
 import time
 
@@ -9,29 +10,64 @@ import time
 # Builder.load_file('Control.kv')
 
 
+def calc_vert(up):
+    up = up
+    x = 666
+    y = 666
+    return x, y
+
+
 class Control(BoxLayout):
     your_time = StringProperty()
-    rotate_value = StringProperty()
-    arm_under_value = StringProperty()
-    arm_above_value = StringProperty()
-    gripper_value = StringProperty()
+    str_rotate = StringProperty()
+    str_arm_under = StringProperty()
+    str_arm_above = StringProperty()
+    str_gripper = StringProperty()
+    str_posy = StringProperty()
+    str_posx = StringProperty()
+    str_posr = StringProperty()
+    # arm_under = 0
+    # arm_above = 0
+    # gripper = 0
 
     def __init__(self, **kwargs):
         super(Control, self).__init__(**kwargs)
         self.orientation = "vertical"
         self.padding = 10
-        self.rotate_value = "10"
-        self.arm_under_value = "10"
-        self.arm_above_value = "10"
-        self.gripper_value = "10"
+        self.posy = 0  # vertical position
+        self.posx = 0  # horizontal position
+        self.posr = rotate = 0  # rotate
+        arm_under = 0
+        arm_above = 0
+        gripper = 0
+        self.str_rotate = str(rotate)
+        self.str_arm_under = str(arm_under)
+        self.str_arm_above = str(arm_above)
+        self.str_gripper = str(gripper)
+        self.str_posy = str(self.posy)
+        self.str_posx = str(self.posx)
+        self.str_posr = str(self.posr)
 
         Clock.schedule_interval(self.set_time, 0.1)
 
     def set_time(self, dt):
         self.your_time = time.strftime("%m/%d/%Y %H:%M")
 
+    def up(self):
+        self.posy = self.posy + 1
+        self.str_posy = str(self.posy)
+        # calculate new values arm_under and arm_above
+        arm_under, arm_above = calc_vert(self.posy)
+        self.str_arm_under = str(arm_under)
+        self.str_arm_above = str(arm_above)
+
+    def down(self):
+        self.posy = self.posy - 1
+        self.str_posy = str(self.posy)
+
 
 class ControlApp(App):
+
     def build(self):
         return Control()
 
